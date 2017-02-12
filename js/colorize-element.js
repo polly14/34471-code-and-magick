eb3dfc3b2fa533f728e07b5168c2228e8191d07d
Polina Bakova <bakovapolina@mail.ru>
2017-02-12 20:34:16 +0300

    инкапсуляция

--- js/colorize-element.js
+++ js/colorize-element.js
@@ -1,21 +1,15 @@
 'use strict';
 
-var ENTER_KEY_CODE = 13;
-
-window.isEnter = function (evt) {
-  return evt.keyCode && evt.keyCode === ENTER_KEY_CODE;
-};
-
 window.colorizeElement = function (element, colors, property) {
   var currentColor = colors[0];
   element.addEventListener('click', function () {
-    var newColor = window.getRandomElementExcept(colors, currentColor);
+    var newColor = window.utils.getRandomElementExcept(colors, currentColor);
     currentColor = newColor;
     element.style[property] = currentColor;
   });
   element.addEventListener('keydown', function (evt) {
-    if (window.isEnter(evt)) {
-      var newColor = window.getRandomElementExcept(colors, currentColor);
+    if (window.utils.isEnter(evt)) {
+      var newColor = window.utils.getRandomElementExcept(colors, currentColor);
       currentColor = newColor;
       element.style[property] = currentColor;
     }
