89b6aa5980fcddde04ac55a0a20e997988c16941
Polina Bakova <bakovapolina@mail.ru>
2017-02-15 23:02:28 +0300

    правки

--- js/colorize-element.js
+++ js/colorize-element.js
@@ -1,23 +1,23 @@
 'use strict';
 
-window.colorizeElement = function (element, colors, callback) {
-  var currentColor = colors[0];
-  element.addEventListener('click', function () {
-    var newColor = window.utils.getRandomElementExcept(colors, currentColor);
-    currentColor = newColor;
-    if (typeof callback === 'function') {
-      callback(element, newColor);
-    }
-  });
-  element.addEventListener('keydown', function (evt) {
-    if (window.utils.isEnter(evt)) {
+(function () {
+  window.colorizeElement = function (element, colors, callback) {
+    var currentColor = colors[0];
+    var colorize = function () {
       var newColor = window.utils.getRandomElementExcept(colors, currentColor);
       currentColor = newColor;
       if (typeof callback === 'function') {
         callback(element, newColor);
       }
-    }
-  });
+    };
+    element.addEventListener('click', function () {
+      colorize();
+    });
+    element.addEventListener('keydown', function (evt) {
+      if (window.utils.isEnter(evt)) {
+        colorize();
+      }
+    });
 
-};
-
+  };
+})();
