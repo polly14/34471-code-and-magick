1383d16b567e241d68ef2b998e1bfa0399c19404
Polina Bakova <bakovapolina@mail.ru>
2017-02-15 20:59:23 +0300

    колбэк

--- js/colorize-element.js
+++ js/colorize-element.js
@@ -1,5 +1,7 @@
 'use strict';
 
+
+/*
 window.colorizeElement = function (element, colors, property) {
   var currentColor = colors[0];
   element.addEventListener('click', function () {
@@ -14,5 +16,27 @@ window.colorizeElement = function (element, colors, property) {
       element.style[property] = currentColor;
     }
   });
+};
+*/
+
+window.colorizeElement = function (element, colors, callback) {
+  var currentColor = colors[0];
+  element.addEventListener('click', function () {
+    var newColor = window.utils.getRandomElementExcept(colors, currentColor);
+    currentColor = newColor;
+    if (typeof callback === 'function') {
+      callback(newColor);
+    }
+  });
+  element.addEventListener('keydown', function (evt) {
+    if (window.utils.isEnter(evt)) {
+      var newColor = window.utils.getRandomElementExcept(colors, currentColor);
+      currentColor = newColor;
+      if (typeof callback === 'function') {
+        callback(newColor);
+      }
+    }
+  });
+           
 };
 
--- js/setup.js
+++ js/setup.js
@@ -1,15 +1,10 @@
 'use strict';
 
 (function () {
-  var setup = document.querySelector('.setup');
-  var setupOpen = document.querySelector('.setup-open');
-  var setupClose = setup.querySelector('.setup-close');
-  var setupSubmit = setup.querySelector('.setup-submit');
+  
   var wizardCoat = document.querySelector('#wizard-coat');
   var wizardEyes = document.querySelector('#wizard-eyes');
   var setupFireballWrap = document.querySelector('.setup-fireball-wrap');
-  var setupWizardForm = setup.querySelector('.setup-wizard-form');
-  var ESCAPE_KEY_CODE = 27;
 
   var wizardCoatColors = [
     'rgb(101, 137, 164)',
@@ -34,17 +29,26 @@
     '#e6e848'
   ];
 
-  window.colorizeElement(wizardCoat, wizardCoatColors, 'fill');
+  window.colorizeElement(wizardCoat, wizardCoatColors, function(currentColor) {wizardCoat.style.fill = currentColor;});
 
-  window.colorizeElement(wizardEyes, wizardEyesColors, 'fill');
+  window.colorizeElement(wizardEyes, wizardEyesColors, function(currentColor) {wizardEyes.style.fill = currentColor;});
 
-  window.colorizeElement(setupFireballWrap, setupFireballWrapColor, 'background');
+  window.colorizeElement(setupFireballWrap, setupFireballWrapColor, function(currentColor) {setupFireballWrap.style.background = currentColor;});
+  
+})();
 
+window.enableSetup = (function () {
+  var setup = document.querySelector('.setup');
+  var setupOpen = document.querySelector('.setup-open');
+  var setupClose = setup.querySelector('.setup-close');
+  var setupSubmit = document.querySelector('.setup-submit');
+  var setupWizardForm = document.querySelector('.setup-wizard-form');  
   var onSetupKeydown = function (evt) {
-    if (evt.keyCode === ESCAPE_KEY_CODE) {
+    if (window.utils.isEscape(evt)) {
       setup.classList.add('invisible');
     }
   };
+  var onSetupClose = null;
 
   var showSetupElement = function () {
     setup.classList.remove('invisible');
@@ -58,39 +62,58 @@
     document.removeEventListener('keydown', onSetupKeydown);
     setupOpen.setAttribute('aria-pressed', 'false');
     setup.setAttribute('aria-hidden', 'true');
+    setupClose.removeEventListener('keydown', onKeyDown);
+    setupClose.removeEventListener('click', onClick);
+    setupSubmit.removeEventListener('click', onClick);
+    setupSubmit.removeEventListener('keydown', onKeyDown);
+
+    if (typeof onSetupClose === 'function') {
+      onSetupClose();
+    }
   };
 
-  setupOpen.addEventListener('click', function () {
-    showSetupElement();
-  });
-
-  setupOpen.addEventListener('keydown', function (evt) {
-    if (window.utils.isEnter(evt)) {
-      showSetupElement();
-    }
-  });
-
-  setupClose.addEventListener('click', function () {
+  var onClick = function () {
     hideSetupElement();
-  });
+  };
 
-  setupClose.addEventListener('keydown', function (evt) {
+  var onKeyDown = function (evt) {
     if (window.utils.isEnter(evt)) {
       hideSetupElement();
     }
-  });
+  };
 
   setupWizardForm.addEventListener('submit', function (evt) {
     evt.preventDefault();
   });
 
-  setupSubmit.addEventListener('click', hideSetupElement);
-
-  setupSubmit.addEventListener('keydown', function (evt) {
-    if (window.utils.isEnter(evt)) {
-      hideSetupElement();
-    }
-  });
+  return function (cb) {
+    showSetupElement();
+    setupClose.addEventListener('keydown', onKeyDown);
+    setupClose.addEventListener('click', onClick);
+    setupSubmit.addEventListener('click', onClick);
+    setupSubmit.addEventListener('keydown', onKeyDown);
+    onSetupClose = cb;
+  };
 
 })();
 
+(function () {
+  var setupOpen = document.querySelector('.setup-open-icon');
+
+  setupOpen.addEventListener('click', function () {
+    window.enableSetup();
+  });
+
+  var focusOpenButton = function () {
+    setupOpen.focus();
+  };
+
+  var onSetupKeydown = function (evt) {
+    if (window.utils.isEnter(evt)) {
+      window.enableSetup(focusOpenButton);
+    }
+  };
+
+  setupOpen.addEventListener('keydown', onSetupKeydown);
+
+})();
